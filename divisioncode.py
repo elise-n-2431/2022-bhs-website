2699157cb2b75b021d8fd2d067bfa96e53f242de
eliseschool <18365@burnside.school.nz>
2021-05-17 13:00:49 +1200

    changed database and divisonal points

--- divisioncode.py
+++ divisioncode.py
@@ -2,13 +2,25 @@ import sqlite3
 first=('test1',0)
 second=('test1',0)
 third=('test1',0)
+north=0
+south=0
+west=0
+divisions=[north,south,west]
 with sqlite3.connect("db/Divisionalpoints.db") as connection:
     cursor=connection.cursor()
-    cursor.execute("SELECT division,value FROM test")
+    cursor.execute("SELECT north, south, west FROM Points")
+    #won't work as database has changed. Need to include something to total all values in each column for the total
     thing=cursor.fetchall()
     for item in thing:
-        if item[1]>first[1]:
-            first=item
+        north=north+item[0]
+        south=south+item[1]
+        west=west+item[2]
+    print(north)
+    print(south)
+    print(west)
+    for division in divisions:
+    if item[1]>first[1]:
+        first=item
     thing.remove(first)
     for item in thing:
         if item[1]>second[1]:
@@ -16,9 +28,3 @@ with sqlite3.connect("db/Divisionalpoints.db") as connection:
     thing.remove(second)
     for item in thing:
         third=item
-    one=first[1]
-    two=second[1]
-    three=third[1]
-    first=first[0]
-    second=second[0]
-    third=third[0]
--- main.py
+++ main.py
@@ -8,10 +8,18 @@ def home():
     first=('test1',0)
     second=('test1',0)
     third=('test1',0)
+    north=0
+    south=0
+    west=0
     with sqlite3.connect("db/Divisionalpoints.db") as connection:
         cursor=connection.cursor()
-        cursor.execute("SELECT division,value FROM test")
+        cursor.execute("SELECT north, south, west FROM Points")
+        #won't work as database has changed. Need to include something to total all values in each column for the total
         thing=cursor.fetchall()
+        for item in thing:
+            north=north+item[0]
+            south=south+item[1]
+            west=west+item[2]
         for item in thing:
             if item[1]>first[1]:
                 first=item
@@ -33,6 +41,14 @@ def home():
 @app.route('/clubs')
 def clubs():
     return render_template("clubs.html")
+
+@app.route('/divpoints')
+def divpoints():
+    with sqlite3.connect("db/Divisionalpoints.db") as connection:
+    #this page is to show the history of all the divisional points as a list and should include a form to add more points hidden behind a login wall. (students shouldn't be able to get in through inspect elements)
+        cursor=connection.cursor()
+        cursor.execute('')
+    return render_template('divisionalpoints.html')
 
 if __name__=="__main__":
     app.run(debug=True)
