4383f7f360d54654dc236b7f033ab1fc18f150ca
eliseschool <18365@burnside.school.nz>
2021-05-18 14:44:34 +1200

    fixed div points so it can change colour

--- divisioncode.py
+++ divisioncode.py
@@ -1,23 +1,31 @@
 import sqlite3
-    first=0; second=0; third=0; north=0; south=0; west=0
-    with sqlite3.connect("db/Divisionalpoints.db") as connection:
-        cursor=connection.cursor()
-        cursor.execute("SELECT north, south, west FROM Points")
-        thing=cursor.fetchall()
-        for item in thing:
-            north=north+item[0]
-            south=south+item[1]
-            west=west+item[2]
-        divisions=[north,south,west]
-        for division in divisions:
-            if division>first:
-                first=division
-        divisions.remove(first)
-        for division in divisions:
-            if division>second:
-                second=division
-        divisions.remove(second)
-        for division in divisions:
-            third=division
-        cursor.execute("SELECT ")
-    one=first; two=second; three=third; first=first[0]; second=second[0];third=third[0]
+first=['north',0]; second=['north',0]; third=['north',0]; north=['north',0]; south=['south',0]; west=['west',0]
+with sqlite3.connect("db/Divisionalpoints.db") as connection:
+    cursor=connection.cursor()
+    cursor.execute("SELECT colour,name FROM Divisions")
+    results=cursor.fetchall()
+    for result in results:
+        if result[1]=='north':
+            north.append(result[0])
+        if result[1]=='south':
+            south.append(result[0])
+        if result[1]=='west':
+            west.append(result[0])
+    cursor.execute("SELECT north, south, west FROM Points")
+    thing=cursor.fetchall()
+    for item in thing:
+        north[1]=north[1]+item[0]
+        south[1]=south[1]+item[1]
+        west[1]=west[1]+item[2]
+    divisions=[north,south,west]
+    for division in divisions:
+        if division[1]>first[1]:
+            first=division
+    divisions.remove(first)
+    for division in divisions:
+        if division>second:
+            second=division
+    divisions.remove(second)
+    for division in divisions:
+        third=division
+    num1=first[1]; num2=second[1]; num3=third[1]; name1=first[0]; name2=second[0]; name3=third[0]; col1=first[2]; col2=second[2]; col3=third[2]
--- main.py
+++ main.py
@@ -5,18 +5,27 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    first=0; second=0; third=0; north=0; south=0; west=0
+    first=['north',0]; second=['north',0]; third=['north',0]; north=['north',0]; south=['south',0]; west=['west',0]
     with sqlite3.connect("db/Divisionalpoints.db") as connection:
         cursor=connection.cursor()
+        cursor.execute("SELECT colour,name FROM Divisions")
+        results=cursor.fetchall()
+        for result in results:
+            if result[1]=='north':
+                north.append(result[0])
+            if result[1]=='south':
+                south.append(result[0])
+            if result[1]=='west':
+                west.append(result[0])
         cursor.execute("SELECT north, south, west FROM Points")
         thing=cursor.fetchall()
         for item in thing:
-            north=north+item[0]
-            south=south+item[1]
-            west=west+item[2]
+            north[1]=north[1]+item[0]
+            south[1]=south[1]+item[1]
+            west[1]=west[1]+item[2]
         divisions=[north,south,west]
         for division in divisions:
-            if division>first:
+            if division[1]>first[1]:
                 first=division
         divisions.remove(first)
         for division in divisions:
@@ -25,8 +34,8 @@ def home():
         divisions.remove(second)
         for division in divisions:
             third=division
-    one=first; two=second; three=third; first=first[0]; second=second[0];third=third[0]
-    return render_template('home.html', first=first,second=second,third=third,one=one,two=two,three=three)
+        num1=first[1]; num2=second[1]; num3=third[1]; name1=first[0]; name2=second[0]; name3=third[0]; col1=first[2]; col2=second[2]; col3=third[2]
+    return render_template('home.html', num1=num1,num2=num2,num3=num3,name1=name1,name2=name2,name3=name3,col1=col1,col2=col2,col3=col3)
 
 @app.route('/clubs')
 def clubs():
