d1f9938c20ee4a2e38e018fab6e0d4d1527932a8
eliseschool <18365@burnside.school.nz>
2021-05-31 12:57:43 +1200

    sidebar height

---
+++ templates/databaseinput.py
@@ -0,0 +1,8 @@
+x=1
+while x<50:
+    import sqlite3
+    with sqlite3.connect("db/Divisionalpoints.db") as connection:
+        cursor=connection.cursor()
+        cursor.execute("INSERT INTO Points (north,south,west,event,date) VALUES (100,100,100,'test',2021-05-31)")
+        print(x)
+        x=x+1
