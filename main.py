21da3217a43462f92263d2434704ab7f4e33e538
eliseschool <18365@burnside.school.nz>
2021-06-23 14:30:30 +1200

    clubs page minor changes

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def home():
 def clubs():
     with sqlite3.connect("db/Clubs.db") as connection:
         cursor=connection.cursor()
-        cursor.execute('SELECT Club.title,Club.room,Club."desc",Club.contact,Club.teachcode,Club.category FROM Club ')
+        cursor.execute('SELECT Club.title,Club.room,Club."desc",Club.contact,Club.teachcode,Club.category,club.restrictions FROM Club ')
         clubs=cursor.fetchall()
         time='SELECT Time.time FROM Club JOIN ClubTime ON ClubTime.cid=Club.id JOIN Time ON Time.id=ClubTime.tid WHERE Club.id=?;'
         day='SELECT Day.day FROM Club JOIN ClubDay ON ClubDay.cid=Club.id JOIN Day ON Day.id=ClubDay.cid WHERE Club.id=?;'
