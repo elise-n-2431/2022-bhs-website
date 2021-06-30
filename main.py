f5201cde46771509ad7b5eb661e937a7b3ec9455
eliseschool <18365@burnside.school.nz>
2021-06-30 15:03:28 +1200

    connected sql to main page

--- main.py
+++ main.py
@@ -4,44 +4,57 @@ import datetime
 
 app=Flask(__name__)
 
+def connectsql(data,query):
+    with sqlite3.connect(data) as connection:
+        cursor=connection.cursor()
+        cursor.execute(query)
+        results=cursor.fetchall()
+        return results
+
 @app.route('/')
 def home():
     #fetch divisional points and determine the total per division and order them
     first=['north',0]; second=['north',0]; third=['north',0]; north=['North',0]; south=['South',0]; west=['West',0]
-    with sqlite3.connect("db/Divisionalpoints.db") as connection:
+    results=connectsql("db/Divisionalpoints.db","SELECT colour,name FROM Divisions")
+    '''with sqlite3.connect("db/Divisionalpoints.db") as connection:
         cursor=connection.cursor()
         cursor.execute("SELECT colour,name FROM Divisions")
-        results=cursor.fetchall()
+        results=cursor.fetchall()'''
         #fetch and list the colour and name for each division
-        for result in results:
-            if result[1]=='north':
-                north.append(result[0])
-            if result[1]=='south':
-                south.append(result[0])
-            if result[1]=='west':
-                west.append(result[0])
-        cursor.execute("SELECT north, south, west FROM Points")
-        thing=cursor.fetchall()
-        #add all the points for each event to find total per division
-        for item in thing:
-            north[1]=north[1]+item[0]; south[1]=south[1]+item[1]; west[1]=west[1]+item[2]
-        divisions=[north,south,west]
-        #find the division with the most points
-        for division in divisions:
-            if division[1]>first[1]:
-                first=division
-        divisions.remove(first)
-        #find the second place division
-        for division in divisions:
-            if division[1]>second[1]:
-                second=division
-        divisions.remove(second)
-        #the last division=third
-        for division in divisions:
-            third=division
-        #define division values, name and colour eg.num1=largest number, name3=name of div with least points etc.
-        num1=first[1]; num2=second[1]; num3=third[1]; name1=first[0]; name2=second[0]; name3=third[0]; col1=first[2]; col2=second[2]; col3=third[2]
-        return render_template('home.html', num1=num1,num2=num2,num3=num3,name1=name1,name2=name2,name3=name3,col1=col1,col2=col2,col3=col3)
+    for result in results:
+        if result[1]=='north':
+            north.append(result[0])
+        if result[1]=='south':
+            south.append(result[0])
+        if result[1]=='west':
+            west.append(result[0])
+    thing=connectsql("db/Divisionalpoints.db","SELECT north, south, west FROM Points")
+    #add all the points for each event to find total per division
+    for item in thing:
+        north[1]=north[1]+item[0]; south[1]=south[1]+item[1]; west[1]=west[1]+item[2]
+    divisions=[north,south,west]
+    #find the division with the most points
+    for division in divisions:
+        if division[1]>first[1]:
+            first=division
+    divisions.remove(first)
+    #find the second place division
+    for division in divisions:
+        if division[1]>second[1]:
+            second=division
+    divisions.remove(second)
+    #the last division=third
+    for division in divisions:
+        third=division
+    #define division values, name and colour eg.num1=largest number, name3=name of div with least points etc.
+    num1=first[1]; num2=second[1]; num3=third[1]; name1=first[0]; name2=second[0]; name3=third[0]; col1=first[2]; col2=second[2]; col3=third[2]
+    contents=connectsql("db/HomePageContent.db","SELECT content,size FROM Content ORDER BY box")
+    content=[]
+    size=[]
+    for item in contents:
+        content.append(item[0])
+        size.append(item[1])
+    return render_template('home.html', num1=num1,num2=num2,num3=num3,name1=name1,name2=name2,name3=name3,col1=col1,col2=col2,col3=col3,content=content,size=size)
 
 @app.route('/clubs')
 #create a page displaying the clubs
@@ -83,5 +96,8 @@ def library():
 @app.route('/links')
 def links():
     return render_template('links.html')
+
+
+
 if __name__=="__main__":
     app.run(debug=True)
