4a452f74fe838db47e8e2d412f28ecb047552a3e
eliseschool <18365@burnside.school.nz>
2021-07-12 22:07:34 +1200

    edits? mixed

--- main.py
+++ main.py
@@ -11,15 +11,19 @@ def connectsql(data,query):
         results=cursor.fetchall()
         return results
 
+week1=datetime.datetime.now()
+week=week1.strftime("%W")
+week=int(week)
+if (week%2) ==0:
+    week='B'
+else:
+    week='A'
+
 @app.route('/')
 def home():
     #fetch divisional points and determine the total per division and order them
     first=['north',0]; second=['north',0]; third=['north',0]; north=['North',0]; south=['South',0]; west=['West',0]
     results=connectsql("db/Divisionalpoints.db","SELECT colour,name FROM Divisions")
-    '''with sqlite3.connect("db/Divisionalpoints.db") as connection:
-        cursor=connection.cursor()
-        cursor.execute("SELECT colour,name FROM Divisions")
-        results=cursor.fetchall()'''
         #fetch and list the colour and name for each division
     for result in results:
         if result[1]=='north':
@@ -48,7 +52,7 @@ def home():
         third=division
     #define division values, name and colour eg.num1=largest number, name3=name of div with least points etc.
     num1=first[1]; num2=second[1]; num3=third[1]; name1=first[0]; name2=second[0]; name3=third[0]; col1=first[2]; col2=second[2]; col3=third[2]
-    contents=connectsql("db/HomePageContent.db","SELECT content,colour,image,width,height,link FROM Content ORDER BY box")
+    contents=connectsql("db/HomePageContent.db","SELECT content,colour,image,width,height,link,target FROM Content ORDER BY box")
     content=[]
     for item in contents:
         if item[2]!=None:
@@ -57,8 +61,7 @@ def home():
             item[4]=item[4]+2
             item=tuple(item)
         content.append(item)
-    print(content)
-    return render_template('home.html', num1=num1,num2=num2,num3=num3,name1=name1,name2=name2,name3=name3,col1=col1,col2=col2,col3=col3,content=content)
+    return render_template('home.html', num1=num1,num2=num2,num3=num3,name1=name1,name2=name2,name3=name3,col1=col1,col2=col2,col3=col3,content=content,week=week)
 
 @app.route('/clubs')
 #create a page displaying the clubs
@@ -101,6 +104,9 @@ def library():
 def links():
     return render_template('links.html')
 
+@app.route('/printing')
+def printing():
+    return render_template('printing.html')
 
 
 if __name__=="__main__":
